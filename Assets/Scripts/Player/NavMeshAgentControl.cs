39a61ed78c6f3a633364187350cd461a457a7f12
En-Huei <79619573+En-Huei@users.noreply.github.com>
2023-11-23 17:15:30 +0800

    NavMeshAgent for Enemies on the ground
    Create the test scene called NavMesh
    Provide 3 Functional Moving Type (recorded by EnemyState):
    Wander, RunAway, Follow

---
+++ Assets/Scripts/Player/NavMeshAgentControl.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum EnemyState
+{
+    Idle,
+    Wander,
+    RunAway,
+    Follow,
+    Dead
+}
+/* This script is used to control the NavMeshAgent of the enemy */
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentControl : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public float wanderRadius = 5f;
+    public float runAwayRadius = 8f;
+    public float zoneRadiusSquare = 20f;
+    public EnemyState state = EnemyState.Idle;
+    public Camera cam;
+    public NavMeshAgent agent;
+    void Start()
+    {
+        if (agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                state = EnemyState.Follow;
+                agent.SetDestination(hit.point);
+            }
+        }
+        else if ((targetPlayer.position - transform.position).sqrMagnitude < zoneRadiusSquare)
+        {
+            state = EnemyState.RunAway;
+        }
+        else
+        {
+            state = EnemyState.Wander;
+        }
+        switch (state)
+        {
+            case EnemyState.Idle:
+                break;
+            case EnemyState.Wander:
+                Wander();
+                break;
+            case EnemyState.RunAway:
+                RunAway();
+                break;
+            case EnemyState.Follow:
+                break;
+            case EnemyState.Dead:
+                break;
+            default:
+                break;
+        }
+    }
+    void Wander()
+    {
+        NavMeshHit navHit;
+        Vector3 randomPoint = transform.position + Random.insideUnitSphere * wanderRadius;
+        if (NavMesh.SamplePosition(randomPoint, out navHit, wanderRadius, -1))
+        {
+            agent.SetDestination(navHit.position);
+        }
+    }
+    void RunAway()
+    {
+        NavMeshHit navHit;
+        Vector3 randomPoint = transform.position + (transform.position - targetPlayer.position).normalized * runAwayRadius;
+        if (NavMesh.SamplePosition(randomPoint, out navHit, runAwayRadius, -1))
+        {
+            agent.SetDestination(navHit.position);
+        }
+    }
+}
